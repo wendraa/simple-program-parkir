f34e36d47bd081de2dc2603c3255a0feb13144cd
Muhammad Ruswendra <muhammadruswendra@gmail.com>
2024-02-22 22:59:31 +0700

    first commit

---
+++ src/main/java/tgl16/pertemuan9/Pertemuan9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package tgl16.pertemuan9;
+
+/**
+ *
+ * @author uwenn
+ */
+public class Pertemuan9 {
+
+    public static void main(String[] args) {
+    }
+}
